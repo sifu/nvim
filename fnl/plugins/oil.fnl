7ad9c5757e109704503f60d2a1615628d762e0a3
Siegmund Führinger <sifu@0xx0.net>
2024-10-31 12:33:02 +0100

    Fix oil setup
    The setting that supresses the confirm for simple edits was in the wrong
    section

--- fnl/plugins/oil.fnl
+++ fnl/plugins/oil.fnl
@@ -4,10 +4,10 @@
               (oil.setup {:columns []
                           :delete_to_trash true
                           :watch_for_changes true
+                          :skip_confirm_for_simple_edits true
                           :float {:padding 4
                                   :max_width 0
                                   :max_height 0
                                   :border "rounded"
                                   :win_options {:winblend 0}
-                                  :skip_confirm_for_simple_edits true
                                   :view_options {:show_hidden true}}})))}]
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,6 @@
 -- [nfnl] Compiled from fnl/plugins/oil.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
   local oil = require("oil")
-  return oil.setup({columns = {}, delete_to_trash = true, watch_for_changes = true, float = {padding = 4, max_width = 0, max_height = 0, border = "rounded", win_options = {winblend = 0}, skip_confirm_for_simple_edits = true, view_options = {show_hidden = true}}})
+  return oil.setup({columns = {}, delete_to_trash = true, watch_for_changes = true, skip_confirm_for_simple_edits = true, float = {padding = 4, max_width = 0, max_height = 0, border = "rounded", win_options = {winblend = 0}, view_options = {show_hidden = true}}})
 end
 return {{"stevearc/oil.nvim", config = _1_}}
