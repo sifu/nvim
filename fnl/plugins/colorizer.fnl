8c4b0e18f883decf421bd6268444889ed741cc85
Siegmund Führinger <sifu@0xx0.net>
2024-11-02 16:17:35 +0100

    Add plugin "colorizer"

---
+++ fnl/plugins/colorizer.fnl
@@ -0,0 +1,4 @@
+[{1 "norcalli/nvim-colorizer.lua"
+  :config (fn []
+            (let [colorizer (require "colorizer")]
+              (colorizer.setup ["css"] {:mode "background"})))}]
---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,6 @@
+-- [nfnl] Compiled from fnl/plugins/colorizer.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  local colorizer = require("colorizer")
+  return colorizer.setup({"css"}, {mode = "background"})
+end
+return {{"norcalli/nvim-colorizer.lua", config = _1_}}
