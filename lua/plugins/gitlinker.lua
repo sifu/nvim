e525ceb1a60ac98ead482fb3611567b6b2cdee36
Siegmund Führinger <sifu@0xx0.net>
2024-12-06 15:59:30 +0100

    Change GitLink to just copy the url instead of opening it

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -1,2 +1,2 @@
 -- [nfnl] Compiled from fnl/plugins/gitlinker.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"linrongbin16/gitlinker.nvim", cmd = "GitLink", opts = {}, keys = {{"<leader>gL", "<cmd>GitLink!<cr>", mode = {"n", "v"}, desc = "GitLink"}}}
+return {"linrongbin16/gitlinker.nvim", cmd = "GitLink", opts = {}, keys = {{"<leader>gL", "<cmd>GitLink<cr>", mode = {"n", "v"}, desc = "GitLink"}}}
