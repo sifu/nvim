89c2a8416c83f654f4b98a08d2390a5e227e86a3
Siegmund Führinger <sifu@0xx0.net>
2024-12-20 09:48:30 +0100

    More Neogit settings

--- fnl/plugins/neogit.fnl
+++ fnl/plugins/neogit.fnl
@@ -1,9 +1,22 @@
+(vim.api.nvim_create_autocmd ["BufNew" "BufEnter" "BufWinEnter"]
+                             {:pattern "*COMMIT_EDITMSG"
+                              :callback (fn []
+                                          (vim.keymap.set "n" "€€"
+                                                          ":w<cr>:bdelete<cr>"
+                                                          {:buffer true
+                                                           :silent true})
+                                          (vim.keymap.set "i" "€€"
+                                                          "<esc>:w<cr>:bdelete<cr>"
+                                                          {:buffer true
+                                                           :silent true}))})
+
 {1 "NeogitOrg/neogit"
  :cmd "Neogit"
  :dependencies ["nvim-lua/plenary.nvim"
                 "sindrets/diffview.nvim"
                 "nvim-telescope/telescope.nvim"]
  :opts {:integrations {:telescope true :diffview true}
+        :disable_hint true
         :graph_style "kitty"
         :kind "floating"
         :commit_editor {:kind "floating" :spell_check false}
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,2 +1,7 @@
 -- [nfnl] Compiled from fnl/plugins/neogit.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"NeogitOrg/neogit", cmd = "Neogit", dependencies = {"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}, opts = {integrations = {telescope = true, diffview = true}, graph_style = "kitty", kind = "floating", commit_editor = {kind = "floating", spell_check = false}, sections = {recent = {folded = false, hidden = false}}}}
+local function _1_()
+  vim.keymap.set("n", "\226\130\172\226\130\172", ":w<cr>:bdelete<cr>", {buffer = true, silent = true})
+  return vim.keymap.set("i", "\226\130\172\226\130\172", "<esc>:w<cr>:bdelete<cr>", {buffer = true, silent = true})
+end
+vim.api.nvim_create_autocmd({"BufNew", "BufEnter", "BufWinEnter"}, {pattern = "*COMMIT_EDITMSG", callback = _1_})
+return {"NeogitOrg/neogit", cmd = "Neogit", dependencies = {"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}, opts = {integrations = {telescope = true, diffview = true}, disable_hint = true, graph_style = "kitty", kind = "floating", commit_editor = {kind = "floating", spell_check = false}, sections = {recent = {folded = false, hidden = false}}}}
