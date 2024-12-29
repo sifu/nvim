9e801ea2820c5ed3900c988418981e939968ccdc
Siegmund Führinger <sifu@0xx0.net>
2024-12-29 10:28:32 +0100

    Add plugin "folke/noice.nvim" Trying it out again, but with a far less intruding config. By removing the optional nvim-notify plugin, the notifications are far less annoying

---
+++ fnl/plugins/noice.fnl
@@ -0,0 +1,4 @@
+{1 "folke/noice.nvim"
+ :event "VeryLazy"
+ :opts {}
+ :dependencies ["MunifTanjim/nui.nvim"]}
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/noice.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"folke/noice.nvim", event = "VeryLazy", opts = {}, dependencies = {"MunifTanjim/nui.nvim"}}
