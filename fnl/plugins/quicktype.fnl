9af226fd86fc0511d9d105b5394566bb69bc1f68
Siegmund Führinger <sifu@0xx0.net>
2025-01-17 13:13:53 +0100

    Add plugin "midoBB/nvim-quicktype"

---
+++ fnl/plugins/quicktype.fnl
@@ -0,0 +1,3 @@
+{1 "midoBB/nvim-quicktype"
+ :cmd "QuickType"
+ :ft ["typescript" "typescriptreact"]}
---
+++ lua/plugins/quicktype.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/quicktype.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"midoBB/nvim-quicktype", cmd = "QuickType", ft = {"typescript", "typescriptreact"}}
