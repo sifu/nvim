f73addbd64226f73280a87c0a931e33ae7ca0e59
Siegmund Führinger <sifu@0xx0.net>
2025-01-21 17:19:54 +0100

    Setup for TailwindCSS

---
+++ fnl/plugins/tailwind-tools.fnl
@@ -0,0 +1,7 @@
+{1 "luckasRanarison/tailwind-tools.nvim"
+ :name "tailwind-tools"
+ :build ":UpdateRemotePlugins"
+ :dependencies ["nvim-treesitter/nvim-treesitter"
+                "nvim-telescope/telescope.nvim"
+                "neovim/nvim-lspconfig"]
+ :opts {}}
---
+++ lua/plugins/tailwind-tools.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/tailwind-tools.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"luckasRanarison/tailwind-tools.nvim", name = "tailwind-tools", build = ":UpdateRemotePlugins", dependencies = {"nvim-treesitter/nvim-treesitter", "nvim-telescope/telescope.nvim", "neovim/nvim-lspconfig"}, opts = {}}
