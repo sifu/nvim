0a362dae87856ca20245a0dbba5cf9ea60e21dd6
Siegmund Führinger <sifu@0xx0.net>
2025-02-24 07:58:02 +0100

    Remove import-cost. Didn't work on my iMac

--- fnl/plugins/import-cost.fnl
+++
@@ -1,1 +0,0 @@
-{1 "barrett-ruth/import-cost.nvim" :build "sh install.sh yarn" :config true}
--- lua/plugins/import-cost.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/import-cost.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"barrett-ruth/import-cost.nvim", build = "sh install.sh yarn", config = true}
