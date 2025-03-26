21499b8f56fd5ddee55fa41ef5f8fc9aeab21216
Siegmund Führinger <sifu@0xx0.net>
2025-03-26 09:08:15 +0100

    Make SuperMaven work with LuaSnips <c-j> keybinding

--- fnl/plugins/luasnip.fnl
+++ fnl/plugins/luasnip.fnl
@@ -1,5 +1,6 @@
 (fn setup []
   (let [ls (require "luasnip")
+        supermaven (require "supermaven-nvim.completion_preview")
         snippets (require "config.snippets")]
     (ls.config.set_config {:history false
                            :updateevents "TextChanged,TextChangedI"
@@ -9,10 +10,16 @@
                     (fn [] (if (ls.expand_or_jumpable) (ls.expand_or_jump)))
                     {:desc "LuaSnip expand/jump" :silent true})
     (vim.keymap.set ["i" "s"] "<c-j>"
-                    (fn [] (if (ls.jumpable -1) (ls.jump -1)))
+                    (fn []
+                      (if (ls.jumpable -1) (ls.jump -1)
+                          (if (supermaven.has_suggestion)
+                              (supermaven.on_accept_suggestion))))
                     {:desc "LuaSnip expand/jump" :silent true})
     (vim.keymap.set ["i" "s"] "<c-l>"
                     (fn [] (if (ls.choice_active) (ls.change_choice 1)))
                     {:desc "LuaSnip choices" :silent true})))
 
-{1 "L3MON4D3/LuaSnip" :config setup}
+{1 "L3MON4D3/LuaSnip"
+ :config setup
+ :dependencies ["supermaven-inc/supermaven-nvim"]
+ :event "VeryLazy"}
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,6 +1,7 @@
 -- [nfnl] Compiled from fnl/plugins/luasnip.fnl by https://github.com/Olical/nfnl, do not edit.
 local function setup()
   local ls = require("luasnip")
+  local supermaven = require("supermaven-nvim.completion_preview")
   local snippets = require("config.snippets")
   ls.config.set_config({updateevents = "TextChanged,TextChangedI", enable_autosnippets = false, history = false})
   snippets.setup()
@@ -16,17 +17,21 @@ local function setup()
     if ls.jumpable(-1) then
       return ls.jump(-1)
     else
-      return nil
+      if supermaven.has_suggestion() then
+        return supermaven.on_accept_suggestion()
+      else
+        return nil
+      end
     end
   end
   vim.keymap.set({"i", "s"}, "<c-j>", _3_, {desc = "LuaSnip expand/jump", silent = true})
-  local function _5_()
+  local function _6_()
     if ls.choice_active() then
       return ls.change_choice(1)
     else
       return nil
     end
   end
-  return vim.keymap.set({"i", "s"}, "<c-l>", _5_, {desc = "LuaSnip choices", silent = true})
+  return vim.keymap.set({"i", "s"}, "<c-l>", _6_, {desc = "LuaSnip choices", silent = true})
 end
-return {"L3MON4D3/LuaSnip", config = setup}
+return {"L3MON4D3/LuaSnip", config = setup, dependencies = {"supermaven-inc/supermaven-nvim"}, event = "VeryLazy"}
