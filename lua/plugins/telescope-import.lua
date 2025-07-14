18686e771fd25dca72b18bff7c4a5da134a093d0
Siegmund Führinger <sifu@0xx0.net>
2025-07-14 06:59:08 +0200

    Update Packages

--- lua/plugins/telescope-import.lua
+++
@@ -1,6 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/telescope-import.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_()
-  local telescope = require("telescope")
-  return telescope.load_extension("import")
-end
-return {"piersolenski/telescope-import.nvim", dependencies = {"nvim-telescope/telescope.nvim"}, config = _1_}
