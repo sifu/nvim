f7803cf09b0c85bdafce0216cce5fc3d965a6081
Siegmund Führinger <sifu@0xx0.net>
2025-08-05 11:41:45 +0200

    Get rid of the ugly underlines for yanked text

--- fnl/plugins/highlighted-yank.fnl
+++ fnl/plugins/highlighted-yank.fnl
@@ -1,1 +1,5 @@
-{1 "machakann/vim-highlightedyank" :event "VeryLazy"}
+{1 "machakann/vim-highlightedyank"
+ :event "VeryLazy"
+ :config (fn []
+           (vim.schedule (fn []
+                           (vim.cmd "highlight! IncSearch cterm=reverse gui=reverse"))))}
--- lua/plugins/highlighted-yank.lua
+++ lua/plugins/highlighted-yank.lua
@@ -1,2 +1,8 @@
--- [nfnl] Compiled from fnl/plugins/highlighted-yank.fnl by https://github.com/Olical/nfnl, do not edit.
-return {"machakann/vim-highlightedyank", event = "VeryLazy"}
+-- [nfnl] fnl/plugins/highlighted-yank.fnl
+local function _1_()
+  local function _2_()
+    return vim.cmd("highlight! IncSearch cterm=reverse gui=reverse")
+  end
+  return vim.schedule(_2_)
+end
+return {"machakann/vim-highlightedyank", event = "VeryLazy", config = _1_}
