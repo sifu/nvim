d465f13e24649f551ef95bcf1ac0415b930c2a43
Siegmund Führinger <sifu@0xx0.net>
2025-08-07 07:36:20 +0200

    Remove plugin "dummulroy/ts-error-translator.nvim" as it is not working with vtsls

--- fnl/plugins/ts-error-translator.fnl
+++
@@ -1,1 +0,0 @@
-{1 "dmmulroy/ts-error-translator.nvim" :ft ["typescript" "typescriptreact"]}
--- lua/plugins/ts-error-translator.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] fnl/plugins/ts-error-translator.fnl
-return {"dmmulroy/ts-error-translator.nvim", ft = {"typescript", "typescriptreact"}}
