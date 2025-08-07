4a76f8d51f90a3417988111ed803d60efce08be4
Siegmund Führinger <sifu@0xx0.net>
2025-08-07 07:31:55 +0200

    Add plugin "dummulroy/ts-error-translator.nvim"

---
+++ fnl/plugins/ts-error-translator.fnl
@@ -0,0 +1,1 @@
+{1 "dmmulroy/ts-error-translator.nvim" :ft ["typescript" "typescriptreact"]}
---
+++ lua/plugins/ts-error-translator.lua
@@ -0,0 +1,2 @@
+-- [nfnl] fnl/plugins/ts-error-translator.fnl
+return {"dmmulroy/ts-error-translator.nvim", ft = {"typescript", "typescriptreact"}}
