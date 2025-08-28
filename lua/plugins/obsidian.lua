3c0349521a5ec46ea1c83ccdb6302301203f008b
Siegmund Führinger <sifu@0xx0.net>
2025-08-28 09:56:49 +0200

    Implement my own smart_action for Obsidian
    It replicates the behavior of the default smart_action, but if the
    cursor is not on a link or tag, it will open the Telescope buffer
    selector.

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,23 +6,35 @@ local function today()
   local today0 = os.date("%Y-%m-%d")
   return ("[[/Daily/" .. today0 .. "|" .. today0 .. "]]")
 end
-local function _2_(ev)
+local function my_smart_action()
+  local obsidian = require("obsidian")
+  local cursor_link = obsidian.api.cursor_link
+  local cursor_tag = obsidian.api.cursor_tag
+  if cursor_link() then
+    return vim.cmd("Obsidian follow_link")
+  elseif cursor_tag() then
+    return vim.cmd("Obsidian tags")
+  else
+    return vim.cmd("Telescope buffers")
+  end
+end
+local function _3_(ev)
   vim.keymap.set("n", "gf", "<cmd>Obsidian follow_link<cr>", {buffer = ev.buf, desc = "Follow Link"})
   vim.keymap.set("n", ",O", "<cmd>Obsidian open<cr>", {buffer = ev.buf, desc = "Open in App"})
   vim.keymap.set("n", ",s", "<cmd>Obsidian quick_switch<cr>", {buffer = ev.buf, desc = "Quick Switch"})
   vim.keymap.set("n", ",b", "<cmd>Obsidian backlinks<cr>", {buffer = ev.buf, desc = "Backlinks"})
   vim.keymap.set("n", ",t", "<cmd>Obsidian tags<cr>", {buffer = ev.buf, desc = "Tags"})
-  return vim.keymap.del("n", "<CR>", {buffer = ev.buf})
+  return vim.keymap.set("n", "<CR>", my_smart_action, {buffer = ev.buf})
 end
-vim.api.nvim_create_autocmd("User", {pattern = "ObsidianNoteEnter", callback = _2_})
-local function _3_(uri)
+vim.api.nvim_create_autocmd("User", {pattern = "ObsidianNoteEnter", callback = _3_})
+local function _4_(uri)
   return vim.ui.open(uri, {cmd = {"open", "-a", "/Applications/Obsidian.app"}})
 end
-local function _4_(spec)
+local function _5_(spec)
   local path = (spec.dir / tostring(spec.title))
   return path:with_suffix(".md")
 end
-local function _5_(note)
+local function _6_(note)
   return core.merge({["date-created"] = today()}, note.metadata)
 end
-return {"obsidian-nvim/obsidian.nvim", dependencies = {"nvim-lua/plenary.nvim"}, opts = {workspaces = {{name = "Main", path = "~/Obsidian/Main"}}, new_notes_location = "Notes", ui = {enable = false}, open = {func = _3_}, completion = {min_chars = 0}, daily_notes = {folder = "Daily"}, note_path_func = _4_, note_frontmatter_func = _5_, legacy_commands = false}}
+return {"obsidian-nvim/obsidian.nvim", dependencies = {"nvim-lua/plenary.nvim"}, opts = {workspaces = {{name = "Main", path = "~/Obsidian/Main"}}, new_notes_location = "Notes", ui = {enable = false}, open = {func = _4_}, completion = {min_chars = 0}, daily_notes = {folder = "Daily"}, note_path_func = _5_, note_frontmatter_func = _6_, legacy_commands = false}}
