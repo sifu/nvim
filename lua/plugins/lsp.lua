5a40a11f63d8e6138e08d74da316d1459fe6e884
Siegmund Führinger <sifu@0xx0.net>
2025-09-19 13:39:22 +0200

    Update Lsp config to use the new builtin capabilities

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,23 +4,41 @@ local function define_signs(prefix)
   local warn = (prefix .. "SignWarn")
   local info = (prefix .. "SignInfo")
   local hint = (prefix .. "SignHint")
-  vim.fn.sign_define(error, {text = "\239\129\151", texthl = error})
-  vim.fn.sign_define(warn, {text = "\239\129\177", texthl = warn})
-  vim.fn.sign_define(info, {text = "\239\129\154", texthl = info})
-  return vim.fn.sign_define(hint, {text = "\239\129\153", texthl = hint})
+  vim.fn.sign_define(error, {text = "", texthl = error})
+  vim.fn.sign_define(warn, {text = "", texthl = warn})
+  vim.fn.sign_define(info, {text = "", texthl = info})
+  return vim.fn.sign_define(hint, {text = "", texthl = hint})
 end
 define_signs("Diagnostic")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local capabilities = cmp_nvim_lsp.default_capabilities()
-local vtsls_opts = {capabilities = capabilities, settings = {complete_function_calls = true, vtsls = {enableMoveToFileCodeAction = true}, typescript = {preferences = {importModuleSpecifier = "non-relative", importModuleSpecifierEnding = "minimal"}, updateImportsOnFileMove = {enabled = "always"}}, suggest = {completeFunctionCalls = true}, inlayHints = {enumMemberValues = {enabled = true}, functionLikeReturnTypes = {enabled = true}, parameterNames = {enabled = "literals"}, parameterTypes = {enabled = true}, propertyDeclarationTypes = {enabled = true}, variableTypes = {enabled = false}}}}
-local cssls_opts = {capabilities = capabilities, settings = {css = {validate = true, lint = {unknownAtRules = "ignore"}}}}
 local function _1_()
   local mason = require("mason")
   return mason.setup()
 end
 local function _2_()
   local mason_lspconfig = require("mason-lspconfig")
-  local lspconfig = require("lspconfig")
-  return mason_lspconfig.setup({ensure_installed = {"fennel_language_server", "vtsls", "cssmodules_ls", "cssls", "tailwindcss", "clojure_lsp"}}, lspconfig.fennel_language_server.setup({capabilities = capabilities, settings = {fennel = {diagnostics = {globals = {"vim"}}}}}), lspconfig.vtsls.setup(vtsls_opts), lspconfig.cssmodules_ls.setup({capabilities = capabilities}), lspconfig.cssls.setup(cssls_opts), lspconfig.tailwindcss.setup({capabilities = capabilities}), lspconfig.clojure_lsp.setup({capabilities = capabilities}))
+  local cmp_nvim_lsp = require("cmp_nvim_lsp")
+  local capabilities = cmp_nvim_lsp.default_capabilities()
+  mason_lspconfig.setup({ensure_installed = {"fennel_language_server", "vtsls", "cssmodules_ls", "cssls", "tailwindcss", "clojure_lsp"}, automatic_enable = false})
+  vim.lsp.config("fennel_language_server", {capabilities = capabilities, settings = {fennel = {diagnostics = {globals = {"vim"}}}}})
+  vim.lsp.config("vtsls", {capabilities = capabilities, settings = {complete_function_calls = true, vtsls = {enableMoveToFileCodeAction = true}, typescript = {preferences = {importModuleSpecifier = "non-relative", importModuleSpecifierEnding = "minimal"}, updateImportsOnFileMove = {enabled = "always"}}, suggest = {completeFunctionCalls = true}, inlayHints = {enumMemberValues = {enabled = true}, functionLikeReturnTypes = {enabled = true}, parameterNames = {enabled = "literals"}, parameterTypes = {enabled = true}, propertyDeclarationTypes = {enabled = true}, variableTypes = {enabled = false}}}})
+  vim.lsp.config("cssmodules_ls", {capabilities = capabilities})
+  vim.lsp.config("cssls", {capabilities = capabilities, settings = {css = {validate = true, lint = {unknownAtRules = "ignore"}}}})
+  vim.lsp.config("tailwindcss", {capabilities = capabilities})
+  vim.lsp.config("clojure_lsp", {capabilities = capabilities})
+  vim.lsp.enable("fennel_language_server")
+  vim.lsp.enable("vtsls")
+  vim.lsp.enable("cssmodules_ls")
+  vim.lsp.enable("cssls")
+  vim.lsp.enable("tailwindcss")
+  vim.lsp.enable("clojure_lsp")
+  local server_configs = {fennel_language_server = {filetypes = {"fennel"}, cmd = {"fennel-language-server"}, settings = {fennel = {diagnostics = {globals = {"vim"}}}}}, vtsls = {filetypes = {"typescript", "javascript", "typescriptreact", "javascriptreact"}, cmd = {"vtsls", "--stdio"}, settings = {complete_function_calls = true}, vtsls = {enableMoveToFileCodeAction = true}, typescript = {preferences = {importModuleSpecifier = "non-relative", importModuleSpecifierEnding = "minimal"}, updateImportsOnFileMove = {enabled = "always"}}, suggest = {completeFunctionCalls = true}, inlayHints = {enumMemberValues = {enabled = true}, functionLikeReturnTypes = {enabled = true}, parameterNames = {enabled = "literals"}, parameterTypes = {enabled = true}, propertyDeclarationTypes = {enabled = true}, variableTypes = {enabled = false}}}, cssls = {filetypes = {"css", "scss", "less"}, cmd = {"vscode-css-language-server", "--stdio"}, settings = {css = {validate = true}, lint = {unknownAtRules = "ignore"}}}, cssmodules_ls = {filetypes = {"typescript", "javascript", "typescriptreact", "javascriptreact"}, cmd = {"cssmodules-language-server"}}, tailwindcss = {filetypes = {"html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte"}, cmd = {"tailwindcss-language-server", "--stdio"}}, clojure_lsp = {filetypes = {"clojure", "clojurescript", "edn"}, cmd = {"clojure-lsp"}}}
+  for server_name, config in pairs(server_configs) do
+    local function _3_()
+      local server_config = vim.tbl_deep_extend("force", {name = server_name, capabilities = capabilities}, config)
+      return vim.lsp.start(server_config)
+    end
+    vim.api.nvim_create_autocmd("FileType", {pattern = config.filetypes, callback = _3_})
+  end
+  return nil
 end
-return {{"neovim/nvim-lspconfig", dependencies = {"williamboman/mason.nvim"}}, {"williamboman/mason.nvim", config = _1_}, {"williamboman/mason-lspconfig.nvim", dependencies = {"williamboman/mason.nvim", "hrsh7th/cmp-nvim-lsp"}, config = _2_}}
+return {{"williamboman/mason.nvim", config = _1_}, {"williamboman/mason-lspconfig.nvim", dependencies = {"williamboman/mason.nvim", "hrsh7th/cmp-nvim-lsp"}, config = _2_}}
