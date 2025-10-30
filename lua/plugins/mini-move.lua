24230c00579248900694c429a1b60179552f657c
Siegmund Führinger <sifu@0xx0.net>
2025-10-30 09:56:58 +0100

    Remove mini-move
    I never use it and it interferes with the standard visual J command.

--- lua/plugins/mini-move.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] fnl/plugins/mini-move.fnl
-return {"nvim-mini/mini.move", event = "VeryLazy", opts = {mappings = {left = "", right = "", down = "J", up = "K", line_left = "", line_right = "", line_down = "", line_up = ""}, options = {reindent_linewise = true}}, version = false}
