dc67e06f433079dc1220ee7e66e198bcd6daf7bd
Siegmund Führinger <sifu@0xx0.net>
2025-11-06 07:34:16 +0100

    Set tiny-inline-diagnostics load event to VeryLazy
    Maybe that will be more reliable?

--- fnl/plugins/tiny-inline-diagnostics.fnl
+++ fnl/plugins/tiny-inline-diagnostics.fnl
@@ -1,7 +1,7 @@
 (vim.diagnostic.config {:virtual_text false})
 
 {1 "rachartier/tiny-inline-diagnostic.nvim"
- :event "LspAttach"
+ :event "VeryLazy"
  :priority 1000
  :opts {:preset "modern" :options {:multilines {:enabled true}}}
  :config true}
--- lua/plugins/tiny-inline-diagnostics.lua
+++ lua/plugins/tiny-inline-diagnostics.lua
@@ -1,3 +1,3 @@
 -- [nfnl] fnl/plugins/tiny-inline-diagnostics.fnl
 vim.diagnostic.config({virtual_text = false})
-return {"rachartier/tiny-inline-diagnostic.nvim", event = "LspAttach", priority = 1000, opts = {preset = "modern", options = {multilines = {enabled = true}}}, config = true}
+return {"rachartier/tiny-inline-diagnostic.nvim", event = "VeryLazy", priority = 1000, opts = {preset = "modern", options = {multilines = {enabled = true}}}, config = true}
