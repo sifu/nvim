c7fa620a82eac103dc02d2e99345f15acac0f79c
Siegmund Führinger <sifu@0xx0.net>
2026-01-21 08:12:37 +0100

    Add plugin 'ruicsh/tailwind-hover.nvim' mapped to '<leader>lk'
    Shows consolidated Tailwind CSS styles applied to the element under the cursor.

---
+++ fnl/plugins/tailwind-hover.fnl
@@ -0,0 +1,4 @@
+{1 "ruicsh/tailwind-hover.nvim"
+ :keys [{1 "<leader>lk" 2 "<cmd>TailwindHover<cr>" :desc "Tailwind: Hover"}]
+ :opts {}
+ :dependencies ["nvim-treesitter/nvim-treesitter"]}
---
+++ lua/plugins/tailwind-hover.lua
@@ -0,0 +1,2 @@
+-- [nfnl] fnl/plugins/tailwind-hover.fnl
+return {"ruicsh/tailwind-hover.nvim", keys = {{"<leader>lk", "<cmd>TailwindHover<cr>", desc = "Tailwind: Hover"}}, opts = {}, dependencies = {"nvim-treesitter/nvim-treesitter"}}
