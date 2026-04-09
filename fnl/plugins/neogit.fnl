4d8a5f698bedd6d06bebe9937f43b58c9cf963ae
Siegmund Führinger <sifu@0xx0.net>
2026-04-09 09:56:42 +0200

    Always toggle the file fold even when cursor is on a hunk

--- fnl/plugins/neogit.fnl
+++ fnl/plugins/neogit.fnl
@@ -1,3 +1,32 @@
+(fn toggle-file-fold []
+  "Toggle the parent file fold, even when cursor is on a hunk.
+   Walks up the component tree to find the Item (file) level."
+  (let [status (require "neogit.buffers.status")
+        instance (status.instance)]
+    (when (and instance instance.buffer instance.buffer.ui)
+      (let [fold (instance.buffer.ui:get_fold_under_cursor)]
+        (when fold
+          ;; Walk up to the Item (file) level, but don't go past it into Section
+          (var target fold)
+          (var found-item false)
+          ;; First check if we can find an Item ancestor
+          (var node target)
+          (while node
+            (when (and node.options (= node.options.tag "Item"))
+              (set target node)
+              (set found-item true))
+            (set node node.parent))
+          ;; If no Item ancestor found (e.g. already on a Section), use original fold
+          (when (not found-item)
+            (set target fold))
+          (if target.options.on_open
+              (target.options.on_open target instance.buffer.ui)
+              (let [(start _) (target:row_range_abs)]
+                (instance.buffer:move_cursor start)
+                (let [(ok _) (pcall vim.cmd "normal! za")]
+                  (when ok
+                    (set target.options.folded (not target.options.folded)))))))))))
+
 {1 "NeogitOrg/neogit"
  :cmd "Neogit"
  :dependencies ["nvim-lua/plenary.nvim"
@@ -10,6 +39,8 @@
                             :graph_style "kitty"
                             :kind "replace"
                             :sections {:recent {:folded false :hidden false}}
-                            :mappings {:status {:<esc> "Close"}
+                            :mappings {:status {:<esc> "Close"
+                                                :<tab> toggle-file-fold
+                                                :za toggle-file-fold}
                                        :commit_editor {"€€" "Submit"}
                                        :commit_editor_I {"€€" "Submit"}}})))}
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,6 +1,47 @@
 -- [nfnl] fnl/plugins/neogit.fnl
-local function _1_()
-  local neogit = require("neogit")
-  return neogit.setup({integrations = {telescope = true, diffview = true}, disable_hint = true, graph_style = "kitty", kind = "replace", sections = {recent = {folded = false, hidden = false}}, mappings = {status = {["<esc>"] = "Close"}, commit_editor = {["\226\130\172\226\130\172"] = "Submit"}, commit_editor_I = {["\226\130\172\226\130\172"] = "Submit"}}})
+local function toggle_file_fold()
+  local status = require("neogit.buffers.status")
+  local instance = status.instance()
+  if (instance and instance.buffer and instance.buffer.ui) then
+    local fold = instance.buffer.ui:get_fold_under_cursor()
+    if fold then
+      local target = fold
+      local found_item = false
+      local node = target
+      while node do
+        if (node.options and (node.options.tag == "Item")) then
+          target = node
+          found_item = true
+        else
+        end
+        node = node.parent
+      end
+      if not found_item then
+        target = fold
+      else
+      end
+      if target.options.on_open then
+        return target.options.on_open(target, instance.buffer.ui)
+      else
+        local start, _ = target:row_range_abs()
+        instance.buffer:move_cursor(start)
+        local ok, _0 = pcall(vim.cmd, "normal! za")
+        if ok then
+          target.options.folded = not target.options.folded
+          return nil
+        else
+          return nil
+        end
+      end
+    else
+      return nil
+    end
+  else
+    return nil
+  end
 end
-return {"NeogitOrg/neogit", cmd = "Neogit", dependencies = {"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}, config = _1_}
+local function _7_()
+  local neogit = require("neogit")
+  return neogit.setup({integrations = {telescope = true, diffview = true}, disable_hint = true, graph_style = "kitty", kind = "replace", sections = {recent = {folded = false, hidden = false}}, mappings = {status = {["<esc>"] = "Close", ["<tab>"] = toggle_file_fold, za = toggle_file_fold}, commit_editor = {["\226\130\172\226\130\172"] = "Submit"}, commit_editor_I = {["\226\130\172\226\130\172"] = "Submit"}}})
+end
+return {"NeogitOrg/neogit", cmd = "Neogit", dependencies = {"nvim-lua/plenary.nvim", "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim"}, config = _7_}
